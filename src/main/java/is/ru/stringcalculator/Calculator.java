13198bd89bcbfdbb0824ab0cd106c5eecb66fd62
rakels13 <rakels13@ru.is>
2015-10-17 13:07:24 +0000

    Added a test for given delimiters and implimented in calculator code

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,7 +6,10 @@ public class Calculator {
 		if(text.equals("")){
 			return 0;
 		}
-		else if(text.contains(",")){
+		if(text.startsWith("//")){
+			return sum(splitByGiven(text));
+		}
+		if(text.contains(",")){
 			return sum(splitNumbers(text));
 		}
 		else 
@@ -16,9 +19,16 @@ public class Calculator {
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
-	
+
 	private static String[] splitNumbers(String numbers){
 	    return numbers.split("[,:\n]");
+	}
+
+	private static String[] splitByGiven(String numbers){
+	    String[] part = numbers.split("\n");
+		String delimiter = part[0].substring(2);
+		numbers = part[1];
+	    return numbers.split(delimiter);
 	}
       
     private static int sum(String[] numbers){
